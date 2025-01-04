ef0cb86edc79d6a7dffe396c3790c949a9c3222b
Simão Gomes Viana <devel@superboring.dev>
2025-01-04 18:07:50 +0000

    Revert "Jelly: Resolve dynamic new tab URLs semi-automatically"
    This reverts commit 68f7000225bec93e3e67a4368cce32ef5a4e3071.
    
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- app/src/main/java/org/lineageos/jelly/webview/ChromeClient.kt
+++ app/src/main/java/org/lineageos/jelly/webview/ChromeClient.kt
@@ -16,14 +16,12 @@ import android.webkit.PermissionRequest
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.Toast
 import org.lineageos.jelly.R
 import org.lineageos.jelly.js.JsManifest
 import org.lineageos.jelly.ui.UrlBarLayout
 import org.lineageos.jelly.utils.SharedPreferencesExt
 import org.lineageos.jelly.utils.TabUtils.openInNewTab
-import kotlin.reflect.cast
 
 internal class ChromeClient(
     private val activity: WebViewExtActivity,
@@ -137,22 +135,7 @@ internal class ChromeClient(
 
         val result = view.hitTestResult
         val url = result.extra
-
-        if (url == null) {
-            val transport = WebView.WebViewTransport::class.cast(resultMsg.obj)
-            val tempWebView = WebView(view.context)
-            tempWebView.webViewClient = object : WebViewClient() {
-                override fun onLoadResource(view: WebView, url: String) {
-                    tempWebView.destroy()
-                    openInNewTab(activity, url, incognito)
-                }
-            }
-            transport.webView = tempWebView
-            resultMsg.sendToTarget()
-            return true
-        }
-
         openInNewTab(activity, url, incognito)
-        return false
+        return true
     }
 }
